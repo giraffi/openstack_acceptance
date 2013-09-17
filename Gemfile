492897389e06c1cf9f89962d57decced345dad5a
sawanoboly <sawanoboriyu@gmail.com>
2013-09-17 18:50:33 +0900

    create config builder

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'thor'
 gem 'rspec'
 gem 'cucumber'
 gem 'guard-cucumber'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,12 @@
 require 'rspec/matchers'
+require 'yaml'
 
 include RSpec::Matchers
+
+config_file = File.expand_path('../../../.os_accept.yml',__FILE__)
+unless File.exists?(config_file)
+  puts 'ConfigFile Not Found. Try `./bin/os_accept init` first.'
+  exit
+end
+$os_config = YAML.load_file(config_file)
+puts $os_config
